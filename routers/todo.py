207f4156c548d6aa8b4c8c2e674b1123a6af7307
koryroot <20197914@itla.edu.do>
2021-06-20 09:35:18 -0400

    rutas

---
+++ routers/todo.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+from models.serverResult import ServerResult
+from models.todo import Todo
+from db import dbdata
+
+router = APIRouter()
+
+@router.get('/todo/list')
+def todo_list():
+    name = dbdata.Todo.names()
+    completed = dbdata.Todo.full()
+
+    if name:
+        1 = [s for s in name]
+        return ServerResult(response=1, message="Lista de cosas")
+    else:
+        return ServerResult(ok = False, message = "Aun no tiene cosas")
+
+@router.post('/cosas/create')
+def cosas_create():
+    pass
