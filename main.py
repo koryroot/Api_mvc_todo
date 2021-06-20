eacfa1fc306f58cb12546e27526c4d21260a1582
koryroot <20197914@itla.edu.do>
2021-06-20 07:45:12 -0400

    api-init

---
+++ main.py
@@ -0,0 +1,16 @@
+# Wendry Koralis
+
+from fastapi import FastAPI
+#from routers import usuario, secrets
+
+app = FastAPI()
+
+#app.include_router(usuario.router, tags=['Usuario'])
+#app.include_router(secrets.router, tags=['Secrets'])
+
+@app.get('/', tags=['Inicio'])
+def read_root():
+    return {
+        'proyecto': 'Api de mvc todo',
+        'docs': app.docs_url
+    }
